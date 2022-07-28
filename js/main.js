4ecedab7de5b2603a00bee7c378a8612667a7742
Boburiy1906 <Boburshoh1906@gmail.com>
2022-07-28 21:32:36 +0500

    add js functions

--- js/main.js
+++ js/main.js
@@ -2,21 +2,25 @@ const elUrlShortener = document.querySelector(".url-shortener");
 const elUrlShortenerForm = document.querySelector(".js-url-shortener-form");
 const elUrlsShortenerResults = document.querySelector(".url-shortener__results")
 const elCopyShortLinkButton = document.querySelector(".js-copy-short-link-button");
+const elChangeCopyButtonCopied = document.querySelector(".");
 
-if(elUrlShortenerForm){
-elUrlShortenerForm.addEventListener("submit", function(evt){
-evt.preventDefault();
+if (elUrlShortenerForm) {
+  elUrlShortenerForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
 
-elUrlsShortenerResults.classList.add("url-shortener__results--open")
-});
+    elUrlsShortenerResults.classList.add("url-shortener__results--open")
+  });
 }
 
-if(elUrlShortener){
-elUrlShortener.addEventListener("click", function(evt){
-if (evt.target.matches(".js-copy-short-link-button")) {
-  //Change button text
-  evt.target.textContent = "Copied!"
-}
+if (elUrlShortener) {
+  elUrlShortener.addEventListener("click", function (evt) {
+    if (evt.target.matches(".js-copy-short-link-button")) {
+      //Change button text
+      evt.target.textContent = "Copied!"
+    }
 
-});
+  });
 }
+// setTimeout(function(){
+//   evt.classList.add('change-copy-button--copied');
+// },3000);
